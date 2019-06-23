f68224b843973e1498e7f4b4e9920033964d3703
Catherine Phillips <catherineep4189@gmail.com>
2019-06-23 15:41:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if {
     var sayHiToGrandma = 'hello'
     sayHiToGrandma.toLowerCase()
     return "I can\`t hear you!"
-  } 
+
 
 }
